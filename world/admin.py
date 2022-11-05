a7214ed2511e0d90b1e0f96ba9faafb33abe090f
niamhkearney <niamhnichearnaigh@gmail.com>
2022-11-05 21:24:26 +0000

    Added pages for signing in, a home page and a map page

--- world/admin.py
+++ world/admin.py
@@ -1,4 +1,6 @@
 from django.contrib.gis import admin
-from .models import WorldBorder
+from .models import WorldBorder, Profile
 
 admin.site.register(WorldBorder, admin.GISModelAdmin)
+admin.site.register(Profile)
+
--- world/views.py
+++ world/views.py
@@ -3,4 +3,9 @@ from django.shortcuts import render
 
 def index(request):
     pass
-    return render(request, 'polls/index.html')
+    return render(request, 'base.html')
+
+
+def maps(request):
+    pass
+    return render(request, 'map.html')
